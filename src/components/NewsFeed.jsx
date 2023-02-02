c4d86f75bf17b32501ffcba8058d8c9d2abc0640
dhramvir73 <dharmvirkumar7321@gmail.com>
2023-02-02 13:14:07 +0530

    added Loading

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -5,13 +5,17 @@ import Coin from "./Coin";
 
 const NewsFeed = () => {
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
       )
       .then((res) => {
         setCoins(res.data);
+        setLoading(false);
       })
       .catch((error) => console.log(error));
   }, []);
@@ -22,19 +26,21 @@ const NewsFeed = () => {
     <div className="news-feed">
       <h2 className="header">Live Crpyto Prices</h2>
       <div className="scroller">
-        {filterCoins.map((coin) => {
-          return (
-            <Coin
-              key={coin.id}
-              name={coin.name}
-              image={coin.image}
-              symbol={coin.symbol}
-              price={coin.current_price}
-              priceChange={coin.price_change_percentage_24h}
-              volume={coin.total_volume}
-            />
-          );
-        })}
+        {loading
+          ? "Loading...."
+          : filterCoins.map((coin) => {
+              return (
+                <Coin
+                  key={coin.id}
+                  name={coin.name}
+                  image={coin.image}
+                  symbol={coin.symbol}
+                  price={coin.current_price}
+                  priceChange={coin.price_change_percentage_24h}
+                  volume={coin.total_volume}
+                />
+              );
+            })}
       </div>
     </div>
   );
